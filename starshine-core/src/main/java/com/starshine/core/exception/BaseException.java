b26fe15eacbc07db53dbea170ad0a97758307395
songtaojie <songtaojie@otw.cn>
2025-08-15 00:06:59 +0800

    feat:添加帮助类

---
+++ starshine-core/src/main/java/com/starshine/core/exception/BaseException.java
@@ -0,0 +1,56 @@
+package com.starshine.core.exception;
+
+import com.starshine.core.utils.StringUtils;
+
+/**
+ * 基础异常
+ * @author startshine
+ */
+public class BaseException extends RuntimeException {
+    /**
+     * 所属模块
+     */
+    private String module;
+
+    /**
+     * 错误码
+     */
+    private String code;
+
+    /**
+     * 错误码对应的参数
+     */
+    private Object[] args;
+
+    /**
+     * 错误消息
+     */
+    private String defaultMessage;
+
+    public BaseException(String module, String code, Object[] args, String defaultMessage) {
+        this.module = module;
+        this.code = code;
+        this.args = args;
+        this.defaultMessage = defaultMessage;
+    }
+
+    public BaseException(String module, String code, Object[] args) {
+        this(module, code, args, null);
+    }
+    public BaseException(String module, String defaultMessage) {
+        this(module, null, null, defaultMessage);
+    }
+    public BaseException(String code, Object[] args) {
+        this(null, code, args, null);
+    }
+    public BaseException(String defaultMessage) {
+        this(null, defaultMessage);
+    }
+
+    public String getMessage(){
+        String message = null;
+        if(StringUtils.isEmpty(code)){
+
+        }
+    }
+}
---
+++ starshine-core/src/main/java/com/starshine/core/utils/MessageUtils.java
@@ -0,0 +1,11 @@
+package com.starshine.core.utils;
+import org.springframework.context.MessageSource;
+/**
+ * 获取i18N资源文件
+ * @author songtaojie
+ */
+public class MessageUtils {
+public static String message(String code,Object... args){
+    MessageSource messageSource =
+}
+}
